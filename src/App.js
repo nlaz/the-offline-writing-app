1aac10eb79cc06b090aab28c072cac8a97e5cb05
nlaz <nikolazaris@utexas.edu>
2018-01-15 19:46:59 -0500

    update app style and pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +12,11 @@ class App extends Component {
     const { value } = e.target;
     this.setState({ value });
     localStorage.setItem("value", value);
+  };
+
+  onReset = () => {
+    this.setState({ value: "" });
+    localStorage.setItem("value", "");
   };
 
   tryRequest = () => {
@@ -37,21 +42,87 @@ class App extends Component {
 
   render() {
     const { online, value } = this.state;
+    const numWords = value.split(" ").length;
     return (
       <div className="App">
+        <hr className="App-border" />
         {online ? (
-          <div>
-            <h1>The offline-only writing app</h1>
-            <p>Looks like you are online. Please disable your network connection to use the app.</p>
-          </div>
+          <Fragment>
+            {value ? (
+              <div className="App-results">
+                <h1 className="App-title" style={{ maxWidth: "400px" }}>
+                  You wrote <span className="text-green">{numWords} words</span> without
+                  distractions.
+                </h1>
+                <span className="text-secondary" style={{ marginTop: "3em" }}>
+                  Export
+                </span>
+                <div className="App-export-group" style={{ marginTop: "1em" }}>
+                  <div className="App-export-item">
+                    <button disabled>
+                      <i className="fa fa-medium fa-3x" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="App-export-item">
+                    <button disabled>
+                      <i className="fa fa-twitter fa-3x" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="App-export-item">
+                    <button disabled>
+                      <i className="fa fa-facebook fa-3x" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="App-export-item">
+                    <button>
+                      <i className="fa fa-download fa-3x" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+                <span className="text-secondary" style={{ marginTop: "2em" }}>
+                  or
+                </span>
+                <div style={{ marginTop: "1em" }}>
+                  <button onClick={this.onReset} className="App-button">
+                    Clear
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="App-landing">
+                <h1 className="App-title">
+                  The <span className="text-green">Offline</span> Writing App
+                </h1>
+                <h3 className="App-subtitle text-secondary">
+                  Disable distractions and focus on writing
+                </h3>
+              </div>
+            )}
+          </Fragment>
         ) : (
-          <textarea
-            style={{ width: "400px", marginTop: "50px" }}
-            placeholder="Get started typing..."
-            value={value}
-            onChange={this.onChange}
-          />
+          <div className="App-home">
+            <div className="App-textarea">
+              <textarea
+                placeholder="Get started typing..."
+                value={value}
+                onChange={this.onChange}
+              />
+            </div>
+          </div>
         )}
+        <span className="App-footer text-hint">
+          {online ? (
+            <Fragment>
+              {value ? (
+                "Turn off your network connection resume writing."
+              ) : (
+                "Turn off your network connection to get started."
+              )}
+            </Fragment>
+          ) : (
+            "Turn on your network connection when finished."
+          )}
+        </span>
       </div>
     );
   }
