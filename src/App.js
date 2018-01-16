4302b05d3d36b7e1e98187a40b1786f5f3e7b059
nlaz <nikolazaris@utexas.edu>
2018-01-15 21:28:49 -0500

    add deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = { online: false, value: localStorage.getItem("value") };
+  state = { online: false, value: localStorage.getItem("value") || "" };
 
   componentWillMount() {
     this.tryRequest();
