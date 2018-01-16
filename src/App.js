945f83a1168a3e22e17981d6de88973551a0f2a9
nlaz <nikolazaris@utexas.edu>
2018-01-16 00:11:08 -0500

    fix html and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
@@ -43,64 +43,11 @@ class App extends Component {
   render() {
     const { online, value } = this.state;
     const numWords = value.split(" ").length;
+
     return (
       <div className="App">
-        <hr className="App-border" />
-        {online ? (
-          <Fragment>
-            {value ? (
-              <div className="App-results">
-                <h1 className="App-title" style={{ maxWidth: "400px" }}>
-                  You wrote <span className="text-green">{numWords} words</span> without
-                  distractions.
-                </h1>
-                <span className="text-secondary" style={{ marginTop: "3em" }}>
-                  Export
-                </span>
-                <div className="App-export-group" style={{ marginTop: "1em" }}>
-                  <div className="App-export-item">
-                    <button disabled>
-                      <i className="fa fa-medium fa-3x" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="App-export-item">
-                    <button disabled>
-                      <i className="fa fa-twitter fa-3x" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="App-export-item">
-                    <button disabled>
-                      <i className="fa fa-facebook fa-3x" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="App-export-item">
-                    <button>
-                      <i className="fa fa-download fa-3x" aria-hidden="true" />
-                    </button>
-                  </div>
-                </div>
-                <span className="text-secondary" style={{ marginTop: "2em" }}>
-                  or
-                </span>
-                <div style={{ marginTop: "1em" }}>
-                  <button onClick={this.onReset} className="App-button">
-                    Clear
-                  </button>
-                </div>
-              </div>
-            ) : (
-              <div className="App-landing">
-                <h1 className="App-title">
-                  The <span className="text-green">Offline</span> Writing App
-                </h1>
-                <h3 className="App-subtitle text-secondary">
-                  Disable distractions and focus on writing
-                </h3>
-              </div>
-            )}
-          </Fragment>
-        ) : (
-          <div className="App-home">
+        <div className="App-content">
+          {!online && (
             <div className="App-textarea">
               <textarea
                 placeholder="Get started typing..."
@@ -108,21 +55,52 @@ class App extends Component {
                 onChange={this.onChange}
               />
             </div>
-          </div>
-        )}
-        <span className="App-footer text-hint">
-          {online ? (
-            <Fragment>
-              {value ? (
-                "Turn off your network connection resume writing."
-              ) : (
-                "Turn off your network connection to get started."
-              )}
-            </Fragment>
-          ) : (
-            "Turn back on your network connection when finished."
           )}
-        </span>
+          {online && (
+            <React.Fragment>
+              {value ? (
+                <React.Fragment>
+                  <h1 className="App-title App-title--results">
+                    You wrote <span className="text-green">{numWords} words</span> without
+                    distractions.
+                  </h1>
+                  <div className="App-button-group">
+                    <button onClick={this.onReset} className="App-button App-button-dark">
+                      Download
+                    </button>
+                    <button onClick={this.onReset} className="App-button">
+                      Start Over
+                    </button>
+                  </div>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <h1 className="App-title">
+                    The <span className="text-green">Offline</span> Writing App
+                  </h1>
+                  <h3 className="App-subtitle text-secondary">
+                    Disable distractions and focus on writing
+                  </h3>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          )}
+        </div>
+        <div className="App-footer">
+          <span className="text-hint">
+            {online ? (
+              <React.Fragment>
+                {value ? (
+                  "Turn off your network connection resume writing."
+                ) : (
+                  "Turn off your network connection to get started."
+                )}
+              </React.Fragment>
+            ) : (
+              "Turn back on your network connection when finished."
+            )}
+          </span>
+        </div>
       </div>
     );
   }
